fcd6653ce25c3253dd5e54974624d839fc27a4b5
Davide Mauri <damauri@microsoft.com>
2018-09-12 10:51:24 -0700

    refactoring

--- azsc.py
+++ azsc.py
@@ -1,88 +1,10 @@
+from lark import Lark
+from transformers.AZSTransformer import AZSTransformer
 import sys
-from lark import Lark, Transformer, Tree
-from lark.lexer import Token
-from handlers.HandlerManager import HandlerManager
-
-class AQLTransformer(Transformer):
-    __handler_manager = HandlerManager()
-
-    def pair(self, (k,v)):
-        return k,v
-
-    def action(self, (s,)):
-        return s[1:-1]
-
-    def azvalue(self, (s,)):
-        return s
-
-    def string(self, (s,)):
-        return '"' + s[1:-1] + '"'
-
-    def azname(self, (s,)):
-        return '"' + s[1:-1] + '"'
-
-    def azobject(self, (s,)):
-        return s
-
-    def use(self, items):
-        if len(items) == 2:
-            k = items[0]
-            v = items[1]
-
-        if len(items) == 3:
-            k = "{0}-{1}".format(items[0], items[1])
-            v = items[2]
-
-        #print("[set {0} = {1}]".format(k, v))
-        self.__handler_manager.set_context(k, v)
-
-    def create(self, items):                
-        cmd = u""
-        
-        objects = []
-        params = []
-        for item in items:
-            if isinstance(item, Token) and item.type == 'OBJECT':
-                objects.append(str(item))
-            elif isinstance(item, Tree):
-                params = item.children
-            else:
-                name = str(item)
-
-        fqon = ''
-
-        for i in range(len(objects), 0, -1):
-            fqon = ' '.join(objects[0:i])
-            if self.__handler_manager.is_handler_available(fqon):
-                break
-
-        if self.__handler_manager.is_handler_available(fqon):
-            handler = self.__handler_manager.get_handler(fqon)
-            cmd += handler.create(objects, name, params)            
-        else:
-            print("***** MISSING HANDLER FOR: '{0}'".format(fqon))             
-
-        print(cmd)
-
-    def execute(self, items):
-        cmd = u"az"
-
-        cmd += " {0} {1}".format(items[0], items[1])                    
-
-        params = [i for i in items if isinstance(i, Tree)][0]
-
-        for param in params.children:
-            cmd += " --{0} {1}".format(param[0], param[1])
-
-        print(cmd)
-
-    def instruction(self, items):    
-        pass
-
 
 def main(adl_file, debug=False):
     print('loading grammar...')
-    with open('aql-b.lark', 'r') as f:
+    with open('azsc.lark', 'r') as f:
         aql_grammar = f.read()
 
     print('loading adl file...')
@@ -101,11 +23,11 @@ def main(adl_file, debug=False):
     print('transpilation results:')
     print
         
-    AQLTransformer().transform(tree)    
+    AZSTransformer().transform(tree)    
     print
 
 def show_help():
-    print("aldc <file.adl> [options]")
+    print("azsc <file.azs> [options]")
     print
     print("options:")
     print(" --debug : print debug info")
@@ -115,6 +37,10 @@ if __name__ == '__main__':
     debug = False
 
     if ( len(sys.argv) < 2 or len(sys.argv) > 3 ):
+        show_help()
+        exit
+
+    if (not sys.argv[1].endswith(".azs")):
         show_help()
         exit
     
---
+++ transformers/AZSTransformer.py
@@ -0,0 +1,79 @@
+from lark import Transformer, Tree
+from lark.lexer import Token
+from handlers.HandlerManager import HandlerManager
+
+class AZSTransformer(Transformer):
+    __handler_manager = HandlerManager()
+
+    def pair(self, (k,v)):
+        return k,v
+
+    def action(self, (s,)):
+        return s[1:-1]
+
+    def azvalue(self, (s,)):
+        return s
+
+    def string(self, (s,)):
+        return '"' + s[1:-1] + '"'
+
+    def azname(self, (s,)):
+        return '"' + s[1:-1] + '"'
+
+    def azobject(self, (s,)):
+        return s
+
+    def use(self, items):
+        if len(items) == 2:
+            k = items[0]
+            v = items[1]
+
+        if len(items) == 3:
+            k = "{0}-{1}".format(items[0], items[1])
+            v = items[2]
+
+        #print("[set {0} = {1}]".format(k, v))
+        self.__handler_manager.set_context(k, v)
+
+    def create(self, items):                
+        cmd = u""
+        
+        objects = []
+        params = []
+        for item in items:
+            if isinstance(item, Token) and item.type == 'OBJECT':
+                objects.append(str(item))
+            elif isinstance(item, Tree):
+                params = item.children
+            else:
+                name = str(item)
+
+        fqon = ''
+
+        for i in range(len(objects), 0, -1):
+            fqon = ' '.join(objects[0:i])
+            if self.__handler_manager.is_handler_available(fqon):
+                break
+
+        if self.__handler_manager.is_handler_available(fqon):
+            handler = self.__handler_manager.get_handler(fqon)
+            cmd += handler.create(objects, name, params)            
+        else:
+            print("***** MISSING HANDLER FOR: '{0}'".format(fqon))             
+
+        print(cmd)
+
+    def execute(self, items):
+        cmd = u"az"
+
+        cmd += " {0} {1}".format(items[0], items[1])                    
+
+        params = [i for i in items if isinstance(i, Tree)][0]
+
+        for param in params.children:
+            cmd += " --{0} {1}".format(param[0], param[1])
+
+        print(cmd)
+
+    def instruction(self, items):    
+        pass
