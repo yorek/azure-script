0b41661e2ec96350c268b0e4d29dc0f65b7334fc
yorek <info@davidemauri.it>
2018-10-12 14:42:18 -0700

    moved samples into samples folder

--- TODO.md
+++
@@ -1,3 +0,0 @@
-# TODO
-
-- Add support to variables and scripts
--- iot.sh
+++
@@ -1,4 +0,0 @@
-az group create --name "dmiottest" --location "eastus"
-az extension add --name "azure-cli-iot-ext"
-az iot hub create --name "dmiothubtest" --sku "F1" --resource-group "dmiottest"
-az iot hub device-identity create --name "mydevice" --hub-name "dmiothubtest"
