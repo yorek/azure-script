7a915d1f6676aed77c19d68eb38b01c0a9eb9ad6
yorek <info@davidemauri.it>
2018-10-12 17:36:54 -0700

    refactorings

--- azsc.py
+++ azsc.py
@@ -6,7 +6,8 @@ import click
 @click.option('--target', default='az', help="transpiling target. only 'az' supported at the moment.")
 @click.option('--debug', is_flag=True, help="Write debug information")
 def cli(script, target, debug):
-    run_parser(script, target, debug)
+    result = run_parser(script, target, debug)
+    print(result)
 
 if __name__ == '__main__':
     cli()
