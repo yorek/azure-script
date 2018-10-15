f48db6b827a8523f79706ef20503b537b8b8cc3e
yorek <info@davidemauri.it>
2018-10-14 22:33:20 -0700

    getting ready for publishing

--- setup.py
+++ setup.py
@@ -1,8 +1,29 @@
-from setuptools import setup
+import setuptools
 
-setup(
-    name='azsc',
-    version='0.1',
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="azsc",
+    version="0.1.0",
+    author="Davide Mauri",
+    author_email="info@davidemauri.it",
+    description="A script language created from AZ CLI commands to make deployment and management of Azure resources as simple and intelligent as possibile.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yorek/azure-script",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Internet",
+        "Topic :: Software Development :: Code Generators",
+        "Topic :: Software Development :: Interpreters",
+        "Topic :: Utilities"
+    ],
     py_modules=['azsc'],
     install_requires=[
         'Click',
