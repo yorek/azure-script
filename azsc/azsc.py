58e1c05784b6b44fd7d297407ee15ec109980c6d
Davide Mauri <damauri@microsoft.com>
2018-10-28 19:29:42 -0700

    made sure az is the only target (for now)

--- azsc/azsc.py
+++ azsc/azsc.py
@@ -8,6 +8,7 @@ import click
 @click.option('--target', default='az', help="transpiling target. only 'az' supported at the moment.")
 @click.option('--debug', is_flag=True, help="Write debug information")
 def cli(script, target, debug):
+    target = "az"
     result = run_parser(script, target, debug)
     print(result)
 
