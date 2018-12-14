cd78b67728942bbfb08bfaedf54c04d5fbe4c168
Davide Mauri <damauri@microsoft.com>
2018-12-13 16:50:32 -0800

    improved eventhubs handler

--- azext_script/compilers/az/handlers/EventHubs.py
+++ azext_script/compilers/az/handlers/EventHubs.py
@@ -5,13 +5,14 @@ class EventHubsHandler(GenericHandler):
     
     def execute(self):
         fqn = self.get_full_resource_name()
-        
+
         if fqn == "eventhubs eventhub consumer-group":
             self.add_context_parameter("eventhub-name", "eventhubs eventhub")
             self.add_context_parameter("namespace-name", "eventhubs namespace")
             self.add_context_parameter("resource-group", "group")
 
         if fqn == "eventhubs eventhub":
+            self.derive_name_from_context("eventhubs eventhub")
             self.add_context_parameter("namespace-name", "eventhubs namespace")
             self.add_context_parameter("resource-group", "group")
 
@@ -20,6 +21,7 @@ class EventHubsHandler(GenericHandler):
             self.add_context_parameter("resource-group", "group")
 
         if fqn == "eventhubs namespace":
+            self.derive_name_from_context("eventhubs namespace")
             self.add_context_parameter("resource-group", "group")
             self.add_context_parameter("location", "location")
 
